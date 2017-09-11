e9dbc8e931d3277e8057077c62399eba7d2c9916
Mark <evenson.not.org@gmail.com>
2017-09-11 14:51:48 +0200

    imported patch abc.diff

--- abc/0.14.6/docker-entrypoint.sh
+++ abc/0.14.6/docker-entrypoint.sh
@@ -11,15 +11,9 @@ if [[ "$1" == "bitcoin-cli" || "$1" == "bitcoin-tx" || "$1" == "bitcoind" || "$1
 		rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
 		rpcuser=${BITCOIN_RPC_USER:-bitcoin}
 		EOF
-		chown bitcoin:bitcoin "$BITCOIN_DATA/bitcoin.conf"
+		chown -R bitcoin:bitcoin $BITCOIN_DATA
 	fi
 
-	# ensure correct ownership and linking of data directory
-	# we do not update group ownership here, in case users want to mount
-	# a host directory and still retain access to it
-	chown -R bitcoin "$BITCOIN_DATA"
-	ln -sfn "$BITCOIN_DATA" /home/bitcoin/.bitcoin
-	chown -h bitcoin:bitcoin /home/bitcoin/.bitcoin
 
 	exec gosu bitcoin "$@"
 fi
