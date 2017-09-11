7e4f732147ec1c9513b3a3d3b1858436682430e9
Mark <evenson.not.org@gmail.com>
2017-09-11 14:51:45 +0200

    imported patch docker-core.diff

--- core/0.14.2/docker-entrypoint.sh
+++ core/0.14.2/docker-entrypoint.sh
@@ -14,13 +14,6 @@ if [[ "$1" == "bitcoin-cli" || "$1" == "bitcoin-tx" || "$1" == "bitcoind" || "$1
 		chown bitcoin:bitcoin "$BITCOIN_DATA/bitcoin.conf"
 	fi
 
-	# ensure correct ownership and linking of data directory
-	# we do not update group ownership here, in case users want to mount
-	# a host directory and still retain access to it
-	chown -R bitcoin "$BITCOIN_DATA"
-	ln -sfn "$BITCOIN_DATA" /home/bitcoin/.bitcoin
-	chown -h bitcoin:bitcoin /home/bitcoin/.bitcoin
-
 	exec gosu bitcoin "$@"
 fi
 
